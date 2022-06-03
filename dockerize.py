1932249c148619ad4f91f96f995d91b75cfe74b6
yoni2 <yoni24536@gmail.com>
2022-06-04 00:57:30 +0300

    python version from venv extraction

--- dockerize.py
+++ dockerize.py
@@ -1,8 +1,17 @@
 import argparse
+from pathlib import Path
+
+from venv_py_props import get_venv_python_version
 
 
 def main():
     args = parse_args()
+    venv_path = Path(args.path)
+
+    if not venv_path.is_dir():
+        raise NotADirectoryError("Given path is invalid")
+
+    venv_py_version = get_venv_python_version(venv_path)
 
 
 def parse_args():
---
+++ venv_py_props.py
@@ -0,0 +1,21 @@
+import configparser
+
+from pathlib import Path
+from packaging.version import Version
+
+
+def get_venv_python_version(path: Path) -> Version:
+    """
+    Extract given venv's python version.
+    :param path: path to venv root folder
+    :return: Version object containing major and minor
+    """
+    section_stub_name = 'section_stub'
+    with open(path / "pyvenv.cfg", 'r') as f:
+        config_string = '[' + section_stub_name + ']\n' + f.read()
+
+    config = configparser.ConfigParser()
+    config.read_string(config_string)
+    major, minor, *_ = config.get(section_stub_name, 'version_info').split(".")
+
+    return Version(major + "." + minor)
