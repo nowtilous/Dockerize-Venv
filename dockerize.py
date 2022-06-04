b5e70656ce1bf4af8e713e66f19cc15c983c9dfc
yoni2 <yoni24536@gmail.com>
2022-06-04 11:43:51 +0300

    dockerfile create

--- dockerize.py
+++ dockerize.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import shutil
 from pathlib import Path
 
 from venv_py_props import get_venv_python_version, collect_pip_dependencies
@@ -7,20 +9,36 @@ from venv_py_props import get_venv_python_version, collect_pip_dependencies
 def main():
     args = parse_args()
     venv_path = Path(args.path)
-
     if not venv_path.is_dir():
         raise NotADirectoryError("Given path is invalid")
+    output_path = venv_path / 'docker'
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
 
     venv_py_version = get_venv_python_version(venv_path)
-    dependencies = collect_pip_dependencies(venv_path)
-    pip_ini = venv_path / 'pip.ini' if (venv_path / 'pip.ini').exists() else None
+    dependencies = collect_pip_dependencies(venv_path, output_path)
+    pip_ini = venv_path / 'pip.ini'
+    if pip_ini.exists():
+        shutil.copyfile(pip_ini, output_path / 'pip.ini')
+    else:
+        open(output_path / 'pip.ini', 'w+')
 
-    print("Python version: {}\nDependencies:\n{}\n".format(venv_py_version, dependencies))
+    print("Python version: {}\nDependencies:".format(venv_py_version))
+    print(*dependencies, sep='')
+
+    with open(output_path / 'dockerfile', 'w') as dockerfile:
+        dockerfile.write("""FROM python:{version}\n\n"""
+                         """COPY requirements.txt requirements.txt\n"""
+                         """COPY pip.ini pip.ini\n\n"""
+                         """RUN pip install -r requirements.txt""".format(version=venv_py_version))
+
+    print("""Docker file created! now build with 'docker build --tag your_image_name {}'\n"""
+          """And then 'docker run -it your_image_name /bin/bash'""".format(output_path))
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', type=str, help='Path to venv root')
+    parser.add_argument('--path', type=str, help='Path to venv root')
     return parser.parse_args()
 
 
--- venv_py_props.py
+++ venv_py_props.py
@@ -1,14 +1,26 @@
 import subprocess
 from pathlib import Path
+from typing import List
 
 from packaging.version import Version
 
 
-def collect_pip_dependencies(path: Path) -> str:
-    return subprocess.check_output([path / 'Scripts' / 'pip.exe', 'freeze'], shell=True).decode()
+def collect_pip_dependencies(path: Path, output_path: Path) -> List[str]:
+    file = open(output_path / 'requirements.txt', 'w+')
+    dep = subprocess.check_output([path / 'Scripts' / 'pip.exe', 'freeze'])
+    file.write(dep.decode())
+
+    file.seek(0)
+    lines = file.readlines()
+    lines = [line for line in lines if line != '\n'][:-1]
+    file.truncate(0)
+    file.seek(0)
+    file.writelines(lines)
+
+    return lines
 
 
 def get_venv_python_version(path: Path) -> Version:
-    ver_string = (subprocess.check_output(
+    version_bytes = (subprocess.check_output(
         [path / 'Scripts' / 'python.exe', '-c', "import platform;print(platform.python_version())"], shell=True))
-    return Version(ver_string.decode())
+    return Version(version_bytes.decode())
